4cf96b4efee318f036cba39028dddf8ac1a95200
Jomari Tiu <85975045+Hohenheimn@users.noreply.github.com>
2023-12-06 16:05:05 +0800

    print template

--- pages/admin/customer/print.tsx
+++ pages/admin/customer/print.tsx
@@ -1,252 +1,227 @@
-import { getCookie } from "cookies-next";
 import React from "react";
+import { getCookie } from "cookies-next";
 import { BarLoader } from "react-spinners";
-import type { customerItemDetail } from "../../../types/customerList";
 import "tippy.js/dist/tippy.css";
-import PrintTemplate from "../../../components/Reusable/PrintTemplate";
+
 import { GetPrintCustomerList } from "../../../components/ReactQuery/CustomerMethod";
-import { PageAccessValidation } from "../../../components/Reusable/PermissionValidation/PageAccessValidation";
 import NoPermissionComp from "../../../components/Reusable/PermissionValidation/NoPermissionComp";
+import { PageAccessValidation } from "../../../components/Reusable/PermissionValidation/PageAccessValidation";
+import PrintTemplate from "../../../components/Reusable/PrintTemplate";
+import type { customerItemDetail } from "../../../types/customerList";
 
 type Props = {
-    Keyword: string;
-    PageNumber: string | number;
-    RowNumber: number;
-    Columns: string;
+  Keyword: string;
+  PageNumber: string | number;
+  RowNumber: number;
+  Columns: string;
 };
 
 export default function Print({
-    Keyword,
-    PageNumber,
-    RowNumber,
-    Columns,
+  Keyword,
+  PageNumber,
+  RowNumber,
+  Columns,
 }: Props) {
-    // Getting column from parameter
-    const ColumnsArray = Columns.split(",");
+  // Getting column from parameter
+  const ColumnsArray = Columns.split(",");
 
-    const { data, isLoading, isError } = GetPrintCustomerList();
+  const { data, isLoading, isError } = GetPrintCustomerList();
 
-    const PagePermisson = PageAccessValidation("Customer");
+  const PagePermisson = PageAccessValidation("Customer");
 
-    if (!PagePermisson && PagePermisson !== undefined) {
-        return <NoPermissionComp />;
-    }
+  if (!PagePermisson && PagePermisson !== undefined) {
+    return <NoPermissionComp />;
+  }
 
-    return (
-        <>
-            <div className="flex items-center flex-col">
-                <PrintTemplate title="Customer">
-                    <table className="w-full">
-                        <thead className="text-[#545454] text-[14px] text-start">
-                            <tr>
-                                <th className="text-start px-2 py-1">ID</th>
-                                <th className="text-start px-2 py-1">NAME</th>
-                                {ColumnsArray.map(
-                                    (item: any, index: number) => (
-                                        <th
-                                            key={index}
-                                            className="text-start px-2 py-1"
-                                            colSpan={
-                                                item === "Property" ? 2 : 1
-                                            }
-                                        >
-                                            {item === "Property" ? (
-                                                <div className="flex">
-                                                    <div className="w-2/4">
-                                                        Property (Unit Code)
-                                                    </div>
-                                                    <div className="w-2/4">
-                                                        Property (Tower)
-                                                    </div>
-                                                </div>
-                                            ) : (
-                                                item
-                                            )}
-                                        </th>
-                                    )
-                                )}
-                            </tr>
-                        </thead>
-                        <tbody className="text-[14px]">
-                            {!isLoading && !isError && (
-                                <>
-                                    {data?.data?.map(
-                                        (item: any, index: number) => (
-                                            <List
-                                                key={index}
-                                                itemDetail={item}
-                                                Columns={ColumnsArray}
-                                            />
-                                        )
-                                    )}
-                                </>
-                            )}
-                        </tbody>
-                    </table>
-                </PrintTemplate>
-                {isLoading && (
-                    <div className="top-0 left-0 absolute w-full h-full flex justify-center items-center">
-                        <aside className="text-center flex justify-center py-5">
-                            <BarLoader
-                                color={"#8f384d"}
-                                height="10px"
-                                width="200px"
-                                aria-label="Loading Spinner"
-                                data-testid="loader"
-                            />
-                        </aside>
-                    </div>
-                )}
-                <button></button>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="flex items-center flex-col">
+        <PrintTemplate title="Customer">
+          <table className="w-full">
+            <thead className="text-[#545454] text-[14px] text-start">
+              <tr>
+                <th className="text-start px-2 py-1">ID</th>
+                <th className="text-start px-2 py-1">NAME</th>
+                {ColumnsArray.map((item: any, index: number) => (
+                  <th
+                    key={index}
+                    className="text-start px-2 py-1"
+                    colSpan={item === "Property" ? 2 : 1}
+                  >
+                    {item === "Property" ? (
+                      <div className="flex">
+                        <div className="w-2/4">Property (Unit Code)</div>
+                        <div className="w-2/4">Property (Tower)</div>
+                      </div>
+                    ) : (
+                      item
+                    )}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody className="text-[14px]">
+              {!isLoading && !isError && (
+                <>
+                  {data?.data?.map((item: any, index: number) => (
+                    <List
+                      key={index}
+                      itemDetail={item}
+                      Columns={ColumnsArray}
+                    />
+                  ))}
+                </>
+              )}
+            </tbody>
+          </table>
+        </PrintTemplate>
+        {isLoading && (
+          <div className="top-0 left-0 absolute w-full h-full flex justify-center items-center">
+            <aside className="text-center flex justify-center py-5">
+              <BarLoader
+                color={"#8f384d"}
+                height="10px"
+                width="200px"
+                aria-label="Loading Spinner"
+                data-testid="loader"
+              />
+            </aside>
+          </div>
+        )}
+        <button></button>
+      </div>
+    </>
+  );
 }
 
 interface ListProps extends customerItemDetail {
-    Columns: any;
+  Columns: any;
 }
 
 const List = ({ itemDetail, Columns }: ListProps) => {
-    let Logo = "/Images/sampleProfile.png";
-    if (itemDetail?.image_photo !== null) {
-        Logo =
-            "https://boroughcrest-api.lws.codes/get-img?image=" +
-            itemDetail?.image_photo;
-    }
+  let Logo = "/Images/sampleProfile.png";
+  if (itemDetail?.image_photo !== null) {
+    Logo =
+      "https://boroughcrest-api.lws.codes/get-img?image=" +
+      itemDetail?.image_photo;
+  }
 
-    return (
-        <tr>
-            <td className=" px-2 py-2 border-b border-gray-300 text-[#2E4364] font-NHU-medium">
-                <div className="flex items-center">
-                    <div>
-                        <p className="font-NHU-medium">{itemDetail?.id}</p>
+  return (
+    <tr>
+      <td className=" px-2 py-2 border-b border-gray-300 text-[#2E4364] font-NHU-medium">
+        <div className="flex items-center">
+          <div>
+            <p className="font-NHU-medium">{itemDetail?.id}</p>
+          </div>
+        </div>
+      </td>
+      <td className=" px-2 py-3 border-b border-gray-300 text-[#2E4364] font-NHU-medium">
+        <p className="font-NHU-medium">{itemDetail?.name}</p>
+      </td>
+      {Columns.map((item: string, index: number) => (
+        <td
+          key={index}
+          className=" px-2 break-words py-1 border-b border-gray-300 text-[#2E4364] font-NHU-medium max-w-[100px]"
+          colSpan={item === "Property" ? 2 : 1}
+        >
+          {item === "Class" && (
+            <p className="font-NHU-medium">{itemDetail?.class}</p>
+          )}
+          {item === "Mobile" && (
+            <p className="font-NHU-medium">{itemDetail?.contact_no}</p>
+          )}
+
+          {item === "Status" && (
+            <p className="font-NHU-medium">{itemDetail?.status}</p>
+          )}
+          {item === "Type" && (
+            <p className="font-NHU-medium">{itemDetail?.type}</p>
+          )}
+          {item === "Email" && (
+            <p className="font-NHU-medium">{itemDetail?.preferred_email}</p>
+          )}
+
+          {item === "Spouse" && (
+            <p className="font-NHU-medium">
+              {itemDetail?.individual_co_owner
+                ? itemDetail?.individual_co_owner
+                : "N/A"}
+            </p>
+          )}
+          {item === "Citizenship" && (
+            <p className="font-NHU-medium">
+              {itemDetail?.individual_citizenship
+                ? itemDetail?.individual_citizenship
+                : "N/A"}
+            </p>
+          )}
+          {item === "Birth Date" && (
+            <p className="font-NHU-medium">
+              {itemDetail?.individual_birth_date
+                ? itemDetail?.individual_birth_date
+                : "N/A"}
+            </p>
+          )}
+          {item === "Contact Person" && (
+            <p className="font-NHU-medium">
+              {itemDetail?.company_contact_person
+                ? itemDetail?.company_contact_person
+                : "N/A"}
+            </p>
+          )}
+          {item === "TIN" && (
+            <p className="font-NHU-medium">{itemDetail?.tin}</p>
+          )}
+          {item === "Branch Code" && (
+            <p className="font-NHU-medium">{itemDetail?.branch_code}</p>
+          )}
+          {item === "Property" && (
+            <>
+              {itemDetail?.properties.map((item: any, index: number) => (
+                <div className="flex" key={index}>
+                  <div className="w-2/4">
+                    <div className="flex flex-wrap">
+                      {item.map((item1: any, index: number) => (
+                        <p
+                          key={index}
+                          className="text-[#2E4364] font-NHU-medium"
+                        >
+                          {item1.unit_code}
+                        </p>
+                      ))}
                     </div>
+                  </div>
+                  <div className="w-2/4">
+                    <div className="flex" key={index}>
+                      <p key={index} className="text-[#2E4364] font-NHU-medium">
+                        {item[0].tower.name}
+                      </p>
+                    </div>
+                  </div>
                 </div>
-            </td>
-            <td className=" px-2 py-3 border-b border-gray-300 text-[#2E4364] font-NHU-medium">
-                <p className="font-NHU-medium">{itemDetail?.name}</p>
-            </td>
-            {Columns.map((item: string, index: number) => (
-                <td
-                    key={index}
-                    className=" px-2 break-words py-1 border-b border-gray-300 text-[#2E4364] font-NHU-medium max-w-[100px]"
-                    colSpan={item === "Property" ? 2 : 1}
-                >
-                    {item === "Class" && (
-                        <p className="font-NHU-medium">{itemDetail?.class}</p>
-                    )}
-                    {item === "Mobile" && (
-                        <p className="font-NHU-medium">
-                            {itemDetail?.contact_no}
-                        </p>
-                    )}
-
-                    {item === "Status" && (
-                        <p className="font-NHU-medium">{itemDetail?.status}</p>
-                    )}
-                    {item === "Type" && (
-                        <p className="font-NHU-medium">{itemDetail?.type}</p>
-                    )}
-                    {item === "Email" && (
-                        <p className="font-NHU-medium">
-                            {itemDetail?.preferred_email}
-                        </p>
-                    )}
-
-                    {item === "Spouse" && (
-                        <p className="font-NHU-medium">
-                            {itemDetail?.individual_co_owner
-                                ? itemDetail?.individual_co_owner
-                                : "N/A"}
-                        </p>
-                    )}
-                    {item === "Citizenship" && (
-                        <p className="font-NHU-medium">
-                            {itemDetail?.individual_citizenship
-                                ? itemDetail?.individual_citizenship
-                                : "N/A"}
-                        </p>
-                    )}
-                    {item === "Birth Date" && (
-                        <p className="font-NHU-medium">
-                            {itemDetail?.individual_birth_date
-                                ? itemDetail?.individual_birth_date
-                                : "N/A"}
-                        </p>
-                    )}
-                    {item === "Contact Person" && (
-                        <p className="font-NHU-medium">
-                            {itemDetail?.company_contact_person
-                                ? itemDetail?.company_contact_person
-                                : "N/A"}
-                        </p>
-                    )}
-                    {item === "TIN" && (
-                        <p className="font-NHU-medium">{itemDetail?.tin}</p>
-                    )}
-                    {item === "Branch Code" && (
-                        <p className="font-NHU-medium">
-                            {itemDetail?.branch_code}
-                        </p>
-                    )}
-                    {item === "Property" && (
-                        <>
-                            {itemDetail?.properties.map(
-                                (item: any, index: number) => (
-                                    <div className="flex" key={index}>
-                                        <div className="w-2/4">
-                                            <div className="flex flex-wrap">
-                                                {item.map(
-                                                    (
-                                                        item1: any,
-                                                        index: number
-                                                    ) => (
-                                                        <p
-                                                            key={index}
-                                                            className="text-[#2E4364] font-NHU-medium"
-                                                        >
-                                                            {item1.unit_code}
-                                                        </p>
-                                                    )
-                                                )}
-                                            </div>
-                                        </div>
-                                        <div className="w-2/4">
-                                            <div className="flex" key={index}>
-                                                <p
-                                                    key={index}
-                                                    className="text-[#2E4364] font-NHU-medium"
-                                                >
-                                                    {item[0].tower.name}
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                )
-                            )}
-                        </>
-                    )}
-                </td>
-            ))}
-        </tr>
-    );
+              ))}
+            </>
+          )}
+        </td>
+      ))}
+    </tr>
+  );
 };
 
 export async function getServerSideProps({ query }: any) {
-    const Keyword = query.keyword;
-    const PageNumber = query.page;
-    const RowNumber = query.limit;
-    const Columns = query.columns;
-    return {
-        props: {
-            PageNumber: PageNumber,
-            RowNumber: RowNumber,
-            Columns: Columns,
-            Keyword: Keyword,
-        },
-    };
+  const Keyword = query.keyword;
+  const PageNumber = query.page;
+  const RowNumber = query.limit;
+  const Columns = query.columns;
+  return {
+    props: {
+      PageNumber: PageNumber,
+      RowNumber: RowNumber,
+      Columns: Columns,
+      Keyword: Keyword,
+    },
+  };
 }
 Print.getLayout = function getLayout(page: any) {
-    return <>{page}</>;
+  return <>{page}</>;
 };
--- pages/finance/customer-facility/collection/payment-register/print.tsx
+++ pages/finance/customer-facility/collection/payment-register/print.tsx
@@ -1,55 +1,92 @@
-import React from "react";
-import { PageAccessValidation } from "../../../../../components/Reusable/PermissionValidation/PageAccessValidation";
+import React, { useEffect, useState } from "react";
+import { startOfDay, format } from "date-fns";
+import Image from "next/image";
+import { BarLoader } from "react-spinners";
+
+import Tippy from "@tippy.js/react";
+
+import { CollectionItem } from ".";
+import ReceiptPrint from "../../../../../components/FINANCE/CustomerFacility/Collection/Print/ReceiptPrint";
+import { GetCollectionDetail } from "../../../../../components/FINANCE/CustomerFacility/Collection/ReceivePayment/Query";
+import { LoginUserInfo } from "../../../../../components/HOC/LoginUser/UserInfo";
 import NoPermissionComp from "../../../../../components/Reusable/PermissionValidation/NoPermissionComp";
-import PrintTemplate from "../../../../../components/Reusable/PrintTemplate";
-import PrintCollectionList from "../../../../../components/FINANCE/CustomerFacility/Collection/PaymentRegister/PrintList";
-import PrintCollectionDetail from "../../../../../components/FINANCE/CustomerFacility/Collection/PaymentRegister/PrintDetail";
+import { PageAccessValidation } from "../../../../../components/Reusable/PermissionValidation/PageAccessValidation";
 
-export default function Print({ payment_register_id, type }: any) {
-    const PagePermisson = PageAccessValidation("Collection");
+export default function Print({
+  payment_register_id,
+  type,
+  receipt_type,
+}: any) {
+  const [userInfo, setUserInfo] = useState<LoginUserInfo>();
+  useEffect(() => {
+    setUserInfo(JSON.parse(localStorage.userInfo));
+  }, []);
+  const printhandler = () => {
+    print();
+  };
 
-    if (!PagePermisson && PagePermisson !== undefined) {
-        return <NoPermissionComp />;
-    }
+  const { isLoading, data } = GetCollectionDetail(payment_register_id);
+  const collection: CollectionItem = data?.data;
 
+  //   Page validation
+  const PagePermisson = PageAccessValidation("Collection");
+  if (!PagePermisson && PagePermisson !== undefined) {
+    return <NoPermissionComp />;
+  }
+  if (isLoading) {
     return (
-        <>
-            <div className="flex items-center flex-col">
-                <PrintTemplate title={type}>
-                    {type !== "payment register list" &&
-                        type !== "payment register detail" && (
-                            <div className="flex justify-center">
-                                <h1>Wrong route</h1>
-                            </div>
-                        )}
-                    {type === "payment register list" && (
-                        <>
-                            <PrintCollectionList />
-                        </>
-                    )}
-                    {type === "payment register detail" && (
-                        <>
-                            <PrintCollectionDetail id={payment_register_id} />
-                        </>
-                    )}
-                </PrintTemplate>
-            </div>
-        </>
+      <div className="top-0 left-0 absolute w-full h-full flex justify-center items-center">
+        <aside className="text-center flex justify-center py-5">
+          <BarLoader
+            color={"#8f384d"}
+            height="10px"
+            width="200px"
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </aside>
+      </div>
     );
+  }
+
+  return (
+    <section className=" flex justify-center text-start">
+      <div className={` w-11/12 max-w-[1366px] printThis print:h-full top-0 `}>
+        <aside className="w-full py-5 flex justify-end px-5 hidePrint print:hidden">
+          <Tippy theme="ThemeRed" content="Print">
+            <div
+              className="relative h-[35px] w-[35px] hover:scale-[1.1] transition-all duration-75"
+              onClick={printhandler}
+            >
+              <Image src="/Images/Print.png" alt="" layout="fill" />
+            </div>
+          </Tippy>
+        </aside>
+
+        <ReceiptPrint
+          userInfo={userInfo}
+          data={collection}
+          receiptType={receipt_type}
+        />
+      </div>
+    </section>
+  );
 }
 
 export async function getServerSideProps({ query }: any) {
-    const type = query?.type;
-    const payment_register_id = query?.payment_register_id;
-    return {
-        props: {
-            type: type,
-            payment_register_id:
-                payment_register_id !== undefined ? payment_register_id : "",
-        },
-    };
+  const type = query?.type;
+  const payment_register_id = query?.payment_register_id;
+  const receipt_type = query?.receipt_type;
+  return {
+    props: {
+      type: type,
+      payment_register_id:
+        payment_register_id !== undefined ? payment_register_id : "",
+      receipt_type: receipt_type ? receipt_type : "",
+    },
+  };
 }
 
 Print.getLayout = function getLayout(page: any) {
-    return <>{page}</>;
+  return <>{page}</>;
 };
